d7392e80a004a5373a0bae46ab73ae4959e87635
O3ObijuanKenobi <karlmcbride1992@gmail.com>
2018-04-08 13:00:24 +0100

    Add application icon

--- Crew_Config_Tool/MainForm.Designer.cs
+++ Crew_Config_Tool/MainForm.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
             this.ListBoxCrews = new System.Windows.Forms.ListBox();
             this.ButtonSortAlpha = new System.Windows.Forms.Button();
             this.ButtonSave = new System.Windows.Forms.Button();
@@ -79,6 +80,7 @@
             this.Controls.Add(this.ButtonSave);
             this.Controls.Add(this.ButtonSortAlpha);
             this.Controls.Add(this.ListBoxCrews);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "MainForm";
             this.Text = "FS Crew Config Tool";
             this.Load += new System.EventHandler(this.MainForm_Load);
