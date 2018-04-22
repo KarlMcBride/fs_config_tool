5b6e5850abd9dc4daff092639566d9d155ddc275
O3ObijuanKenobi <karlmcbride1992@gmail.com>
2018-04-22 15:09:31 +0100

    Add sanity check unit tests for listings

---
+++ Crew_Config_Tool/UnitTests/ListingChecks/CrewAndImplants.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using FS_Crew_Config_Tool;
+using FS_Crew_Config_Tool.Classes;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTests.ListingChecks
+{
+    [TestClass]
+    public class CrewAndImplants
+    {
+        [TestInitialize]
+        public void PopulateLists()
+        {
+            CrewList.PopulateCrewList();
+            ImplantList.PopulateImplantList();
+        }
+
+        [TestMethod]
+        public void VerifyCrewCountsMatch()
+        {
+            int enum_expected = (int)CrewEnum.NONE;
+
+            int actual = CrewList.CrewListing.Count;
+
+            Assert.AreEqual(enum_expected, actual);
+        }
+
+        [TestMethod]
+        public void VerifyImplantCountsMatch()
+        {
+            int enum_expected = (int)ImplantEnum.NONE;
+
+            int actual = ImplantList.ImplantListing.Count;
+
+            Assert.AreEqual(enum_expected, actual);
+        }
+    }
+}
