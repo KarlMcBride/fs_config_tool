4b3bd06f86694a18d8697c3868848c7a51bcf084
O3ObijuanKenobi <karlmcbride1992@gmail.com>
2018-04-23 18:24:44 +0100

    Add CrewBox/CrewSetBox tests to nudge up coverage

--- Crew_Config_Tool/UnitTests/UiComponents/CrewBox_Test.cs
+++ Crew_Config_Tool/UnitTests/UiComponents/CrewBox_Test.cs
@@ -1,6 +1,8 @@
-﻿using FS_Crew_Config_Tool.Classes;
+﻿using FS_Crew_Config_Tool;
+using FS_Crew_Config_Tool.Classes;
 using FS_Crew_Config_Tool.UiComponents;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
 
 namespace UnitTests.UiComponents
 {
@@ -11,14 +13,29 @@ namespace UnitTests.UiComponents
         [TestMethod]
         public void CrewBox_Load()
         {
-            CrewBox asdf = new CrewBox();
-            var aaa = asdf.CallHiddenMethod("Return4");
+            CrewBox crewBox = new CrewBox();
+
+            EventArgs e = new EventArgs();
         }
 
         [TestMethod]
         public void DisplaySelectedCrew_Empty()
         {
             TeamConfig.EnumeratedCrewMember enumeratedCrew = new TeamConfig.EnumeratedCrewMember();
+
+            CrewBox crewBox = new CrewBox();
+            crewBox.DisplaySelectedCrew(enumeratedCrew);
+        }
+
+        [TestMethod]
+        public void DisplaySelectedCrew_Full()
+        {
+            TeamConfig.EnumeratedCrewMember enumeratedCrew = new TeamConfig.EnumeratedCrewMember();
+
+            enumeratedCrew.CrewID = CrewEnum.DICE_CAPLAN;
+            enumeratedCrew.ImplantIDs[0] = ImplantEnum.DAMAGE_REDUCTION;
+            enumeratedCrew.ImplantIDs[1] = ImplantEnum.MANEUVERING;
+            enumeratedCrew.ImplantIDs[2] = ImplantEnum.REPAIR_EFFICIENCY;
 
             CrewBox crewBox = new CrewBox();
             crewBox.DisplaySelectedCrew(enumeratedCrew);
---
+++ Crew_Config_Tool/UnitTests/UiComponents/CrewSetBox_Test.cs
@@ -0,0 +1,28 @@
+﻿using FS_Crew_Config_Tool.UiComponents;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using UnitTests.ConfigParsing.TestData;
+
+namespace UnitTests.UiComponents
+{
+    [TestClass]
+    public class CrewSetBox_Test
+    {
+        [TestMethod]
+        public void CrewBox_Load()
+        {
+            CrewBox crewBox = new CrewBox();
+
+            EventArgs e = new EventArgs();
+
+            crewBox.CallHiddenMethod("CrewBox_Load", crewBox, e);
+        }
+
+        [TestMethod]
+        public void DisplayFullCrew()
+        {
+            CrewSetBox crewSetBox = new CrewSetBox();
+            crewSetBox.DisplaySelectedTeam(ParsedData.FiveMembersFullImplantsOrdered(), null);
+        }
+    }
+}
