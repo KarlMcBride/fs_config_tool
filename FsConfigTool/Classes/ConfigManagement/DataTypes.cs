038a3e4b25bde893bcf31d7d2c1c8b094594d773
O3ObijuanKenobi <karlmcbride1992@gmail.com>
2018-05-13 19:44:44 +0100

    Generate start of crew strings if new

--- FsConfigTool/Classes/ConfigManagement/DataTypes.cs
+++ FsConfigTool/Classes/ConfigManagement/DataTypes.cs
@@ -1,5 +1,6 @@
 ﻿namespace FS_Config_Tool.Classes.ConfigManagement
 {
+    using System;
     using System.Text.RegularExpressions;
 
     namespace FS_Crew_Config_Tool.Classes.ConfigManagement
@@ -36,11 +37,43 @@
 
             public string BuildLine()
             {
-                string line = ConfigUtilities.GetStartOfRawCrewString(RawLine);
+                string line = string.Empty;
+
+                if (RawLine == null)
+                {
+                    // If RawLine is null, this is a new crew, and we'll have to generate the start of the line
+                    line = "CrewTeams=(ID=\"";
+
+                    line += GenerateHexString(32);
+
+                    line += "\",Name=\"" + CrewName + "\",Icon=\"";
+
+                }
+                else
+                {
+                    line = ConfigUtilities.GetStartOfRawCrewString(RawLine);
+                }
+
                 line += CrewBuilder.GenerateCrewStringFromEnumerations(Team);
                 line += ",Members=)";
 
                 return line;
+            }
+
+            private static string GenerateHexString(int length)
+            {
+                const string pool = "0123456789ABCDEF";
+                string returnString = string.Empty;
+
+                Random rng = new Random();
+
+                for (int index = 0; index < length; index++)
+                {
+                    char newChar = pool[rng.Next(0, pool.Length)];
+                    returnString += newChar;
+                }
+
+                return returnString;
             }
         }
     }
