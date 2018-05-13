84cb9d164a54585e66540ced1b24161ba2e3b9d0
O3ObijuanKenobi <karlmcbride1992@gmail.com>
2018-05-13 14:37:23 +0100

    Add feedback and GitHub links

--- Crew_Config_Tool/UiComponents/AboutForm.Designer.cs
+++ Crew_Config_Tool/UiComponents/AboutForm.Designer.cs
@@ -30,6 +30,10 @@
         {
             this.PictureBoxObi = new System.Windows.Forms.PictureBox();
             this.LabelVersion = new System.Windows.Forms.Label();
+            this.LinkLabelFeedback = new System.Windows.Forms.LinkLabel();
+            this.LabelFeedback = new System.Windows.Forms.Label();
+            this.LabelOpenSource = new System.Windows.Forms.Label();
+            this.LinkLabelGitHub = new System.Windows.Forms.LinkLabel();
             ((System.ComponentModel.ISupportInitialize)(this.PictureBoxObi)).BeginInit();
             this.SuspendLayout();
             // 
@@ -37,9 +41,9 @@
             // 
             this.PictureBoxObi.BackgroundImage = global::FS_Crew_Config_Tool.Properties.Resources.ObiwanDog;
             this.PictureBoxObi.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
-            this.PictureBoxObi.Location = new System.Drawing.Point(342, 139);
+            this.PictureBoxObi.Location = new System.Drawing.Point(296, 12);
             this.PictureBoxObi.Name = "PictureBoxObi";
-            this.PictureBoxObi.Size = new System.Drawing.Size(144, 132);
+            this.PictureBoxObi.Size = new System.Drawing.Size(141, 132);
             this.PictureBoxObi.TabIndex = 0;
             this.PictureBoxObi.TabStop = false;
             // 
@@ -52,17 +56,61 @@
             this.LabelVersion.TabIndex = 1;
             this.LabelVersion.Text = "Version: - - - -";
             // 
+            // LinkLabelFeedback
+            // 
+            this.LinkLabelFeedback.AutoSize = true;
+            this.LinkLabelFeedback.Location = new System.Drawing.Point(73, 66);
+            this.LinkLabelFeedback.Name = "LinkLabelFeedback";
+            this.LinkLabelFeedback.Size = new System.Drawing.Size(28, 13);
+            this.LinkLabelFeedback.TabIndex = 2;
+            this.LinkLabelFeedback.TabStop = true;
+            this.LinkLabelFeedback.Text = "here";
+            this.LinkLabelFeedback.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkLabelFeedback_LinkClicked);
+            // 
+            // LabelFeedback
+            // 
+            this.LabelFeedback.Location = new System.Drawing.Point(25, 53);
+            this.LabelFeedback.Name = "LabelFeedback";
+            this.LabelFeedback.Size = new System.Drawing.Size(248, 28);
+            this.LabelFeedback.TabIndex = 3;
+            this.LabelFeedback.Text = "Feedback, suggestions and bug reports are welcome";
+            // 
+            // LabelOpenSource
+            // 
+            this.LabelOpenSource.Location = new System.Drawing.Point(25, 101);
+            this.LabelOpenSource.Name = "LabelOpenSource";
+            this.LabelOpenSource.Size = new System.Drawing.Size(262, 43);
+            this.LabelOpenSource.TabIndex = 4;
+            this.LabelOpenSource.Text = "Thank you for taking the time to try out my config tool. This project is hosted o" +
+    "n ";
+            // 
+            // LinkLabelGitHub
+            // 
+            this.LinkLabelGitHub.AutoSize = true;
+            this.LinkLabelGitHub.Location = new System.Drawing.Point(146, 115);
+            this.LinkLabelGitHub.Name = "LinkLabelGitHub";
+            this.LinkLabelGitHub.Size = new System.Drawing.Size(40, 13);
+            this.LinkLabelGitHub.TabIndex = 5;
+            this.LinkLabelGitHub.TabStop = true;
+            this.LinkLabelGitHub.Text = "GitHub";
+            this.LinkLabelGitHub.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.LinkLabelGitHub_LinkClicked);
+            // 
             // AboutForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(449, 158);
+            this.Controls.Add(this.LinkLabelGitHub);
+            this.Controls.Add(this.LabelOpenSource);
+            this.Controls.Add(this.LinkLabelFeedback);
             this.Controls.Add(this.LabelVersion);
             this.Controls.Add(this.PictureBoxObi);
+            this.Controls.Add(this.LabelFeedback);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "AboutForm";
             this.ShowIcon = false;
+            this.ShowInTaskbar = false;
             this.Text = "About";
             ((System.ComponentModel.ISupportInitialize)(this.PictureBoxObi)).EndInit();
             this.ResumeLayout(false);
@@ -74,5 +122,9 @@
 
         private System.Windows.Forms.PictureBox PictureBoxObi;
         private System.Windows.Forms.Label LabelVersion;
+        private System.Windows.Forms.LinkLabel LinkLabelFeedback;
+        private System.Windows.Forms.Label LabelFeedback;
+        private System.Windows.Forms.Label LabelOpenSource;
+        private System.Windows.Forms.LinkLabel LinkLabelGitHub;
     }
 }
--- Crew_Config_Tool/UiComponents/AboutForm.cs
+++ Crew_Config_Tool/UiComponents/AboutForm.cs
@@ -1,15 +1,34 @@
-﻿using System.Reflection;
+﻿using System.Diagnostics;
+using System.Reflection;
 using System.Windows.Forms;
 
 namespace FS_Crew_Config_Tool.UiComponents
 {
     public partial class AboutForm : Form
     {
+        private string softwareVersion;
+
         public AboutForm()
         {
             InitializeComponent();
 
-            LabelVersion.Text = "Version: " + Assembly.GetExecutingAssembly().GetName().Version;
+            softwareVersion = Assembly.GetExecutingAssembly().GetName().Version.ToString();
+
+            LabelVersion.Text = "Version: " + softwareVersion;
+        }
+
+        private void LinkLabelFeedback_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            string feedbackFormAddress = "https://docs.google.com/forms/d/e/1FAIpQLSfhzr-7Iz3vfyJrAUggUdf7VNO1y4A6V4vVpxiSgIqXkO5nug/viewform?entry.1235937830="
+                                         + softwareVersion;
+
+            Process.Start(feedbackFormAddress);
+        }
+
+        private void LinkLabelGitHub_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            string gitHubAddress = "https://github.com/O3ObijuanKenobi/FracturedSpace";
+            Process.Start(gitHubAddress);
         }
     }
 }
